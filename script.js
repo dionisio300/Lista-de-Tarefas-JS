94d5538de3a39b14ded23cbad38d6b3ac43439d4
Káthia Rocha <125410101+techcomkathia@users.noreply.github.com>
2023-08-28 21:05:55 -0300

    renderizarTarefas

--- script.js
+++ script.js
@@ -5,6 +5,49 @@ const listaTarefas = document.getElementById('listaTarefas');
 // Recupera a lista de tarefas do LocalStorage, se existir
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
+
+// Função para renderizar a lista de tarefas
+function renderizarTarefas() {
+    listaTarefas.innerHTML = '';
+    tarefas.forEach(function(tarefa, index) {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <span class="textoTarefa">${tarefa}</span>
+        <button class="botaoEditar" data-index="${index}">Editar</button>
+        <button class="botaoExcluir" data-index="${index}">Excluir</button>
+      `;
+      listaTarefas.appendChild(listItem);
+    });
+  
+    // Adicionar eventos para os botões de editar e excluir tarefa
+    const botoesEditar = document.querySelectorAll('.botaoEditar');
+    botoesEditar.forEach(function(botao) {
+      botao.addEventListener('click', function(event) {
+        const index = event.target.getAttribute('data-index');
+        const elementoTextoTarefa = event.target.parentElement.querySelector('.textoTarefa');
+        const novoTextoTarefa = prompt('Editar tarefa:', tarefas[index]);
+        if (novoTextoTarefa !== null) {
+          tarefas[index] = novoTextoTarefa.trim();
+          salvarTarefasNoLocalStorage();
+          elementoTextoTarefa.textContent = novoTextoTarefa;
+        }
+      });
+    });
+  
+    const botoesExcluir = document.querySelectorAll('.botaoExcluir');
+    botoesExcluir.forEach(function(botao) {
+      botao.addEventListener('click', function(event) {
+        const index = event.target.getAttribute('data-index');
+        tarefas.splice(index, 1);
+        salvarTarefasNoLocalStorage();
+        renderizarTarefas();
+      });
+    });
+}
+  
+
+
+
 // Função para adicionar uma tarefa
 function adicionarTarefa(tarefa) {
     tarefas.push(tarefa);
@@ -28,4 +71,5 @@ formularioTarefa.addEventListener('submit', function(event) {
     }
 });
 
+renderizarTarefas();
   
